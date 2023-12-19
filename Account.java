8c9b165e596c79d95dd67c7dd2c6fd8c97b4447c
mose.park <mose.park@aol.com>
2023-12-19 22:57:35 +0900

    sublogin logic added, debug modified

--- Account.java
+++ Account.java
@@ -2,31 +2,41 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Account {
-    public static boolean login (String username, String password){
-        if(username.equals("admin") && password.equals("passadmin")){
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public static boolean login(String username, String password) {
+		if (username.equals("admin") && password.equals("passadmin")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public static void loginfail (boolean admin){
-        if(admin == true){
-            System.out.println("Login success, welcome Admin");
-        } else {
-            System.out.println("USER NOT ALLOWED");
-        }
-    }
+	public static void loginfail(boolean admin) {
+		if (admin == true) {
+			System.out.println("Login success, welcome Admin");
+		} else {
+			System.out.println("USER NOT ALLOWED");
+		}
+	}
 
-    public static void main(String[] args) throws Exception{
+	public static void sublogin(String username, String password) {
+		password = "********";
+		System.out.println(username + "is not an admin. Password is encrypted as " + password);
+	}
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String username = br.readLine();
 		String password = br.readLine();
-		
+
 		System.out.println("result :");
-		
-		loginfail(login(username,password));
+
+		if (login(username, password) == true) {
+			loginfail(login(username, password));
+		} else {
+			sublogin(username, password);
+		}
+
 	}
 
 }
