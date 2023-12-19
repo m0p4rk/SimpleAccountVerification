9fa80dc23fe4d508365c31af3832cc190ca41bd8
mose.park <mose.park@aol.com>
2023-12-19 22:49:31 +0900

    debug main added (Step 3)

--- Account.java
+++ Account.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Account {
     public static boolean login (String username, String password){
         if(username.equals("admin") && password.equals("passadmin")){
@@ -14,5 +17,16 @@ public class Account {
             System.out.println("USER NOT ALLOWED");
         }
     }
-    
+
+    public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String username = br.readLine();
+		String password = br.readLine();
+		
+		System.out.println("result :");
+		
+		loginfail(login(username,password));
+	}
+
 }
